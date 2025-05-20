93df6f918a58f4f71e8ab9ee94e8e15934e9d06c
BruyneHuu <buinghialk180303@gmail.com>
2025-05-20 17:49:26 +0700

    feat: get acquainted with type annotations of typescripts

--- projects/lesson6/lesson6.ts
+++ projects/lesson6/lesson6.ts
@@ -1,7 +1,11 @@
-let count2: number; //ok
+// let count2: number; //ok
 
-count2 = 10;
-// count2 = "abc"; // err: Type 'string' is not assignable to type 'number'
+// count2 = 10;
+// // count2 = "abc"; // err: Type 'string' is not assignable to type 'number'
 
-console.log(">>Check count 2:", count2);
+// console.log(">>Check count 2:", count2);
 
+let names: string[] = ['Bruyne', 'Long', 'Phong'];
+// names.push(25); // err
+names.push("25");
+console.log(">>>Check arr: ", names);
